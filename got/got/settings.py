34fd68e76d33fa9304b20842aa92ce0264dddad8
akshaya-b <55309585+akshaya-b@users.noreply.github.com>
2020-02-26 10:53:31 +0000

    Update settings.py

--- got/got/settings.py
+++ got/got/settings.py
@@ -80,8 +80,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'kingdom',
-        'USER': 'postgres',
-        'PASSWORD': '1411',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': 'localhost'
     }
 }
